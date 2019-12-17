6de74665eee4909e3c78dc01ee9478124dabbf3c
Benjo <beniamino.tartarini@gmail.com>
2019-12-17 16:18:28 +0900

    wire review routes

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const postsRouter = require('./routes/posts');
-const usersRouter = require('./routes/users');
+const indexRouter   = require('./routes/index');
+const postsRouter   = require('./routes/posts');
+const reviewsRouter = require('./routes/reviews');
+const usersRouter   = require('./routes/users');
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
+app.use('/posts/:id/reviews', reviewsRouter);
 app.use('/users', usersRouter);
 
 
---
+++ routes/reviews.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true});
+
+/* GET reviews index /posts/id:/reviews */
+router.get('/', (req, res, next) => {
+    res.send("INDEX /posts/:id/reviews");
+  });
+
+    /* POST reviews create /posts/id:/reviews */
+router.post('/', (req, res, next) => {
+    res.send("CREATE /posts/:id/reviews/");
+  });
+    
+  /* GET reviews show /posts/id:/reviews/:review_id */
+router.get('/:review_id', (req, res, next) => {
+    res.send("SHOW /posts/:id/reviews/:review_id");
+  });
+
+    /* GET reviews edit /posts/id:/reviews/:review_id/edit */
+router.get('/:review_id/edit', (req, res, next) => {
+    res.send("EDIT /posts/:id/reviews/:review_id/edit");
+  });
+
+    /* PUT reviews update /posts/id:/reviews/:review_id */
+router.put('/:review_id', (req, res, next) => {
+    res.send("UPDATE /posts/:id/reviews/:review_id/");
+  });
+
+    /* DELETE reviews destroy /posts/id:/reviews/:review_id */
+router.delete('/:review_id', (req, res, next) => {
+    res.send("DESTROY /posts/:id/reviews/:review_id/");
+  });
+
+  module.exports = router;
