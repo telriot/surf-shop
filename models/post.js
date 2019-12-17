c45a4e5fb3bf139179ab6c7d7e68101ed06ad515
Benjo <beniamino.tartarini@gmail.com>
2019-12-17 17:17:56 +0900

    add review and post models

--- models/post.js
+++ models/post.js
@@ -1,12 +1,24 @@
-/* 
-Post
-- title -string
-- price -string
-- description -string
-- images -array of strings
-- location -string
-- lat -number
-- lng -number
-- author -object id (refUser)
-- reviews -array of objects
-*/
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+	title: String,
+	price: String,
+	description: String,
+	images: [ String ],
+	location: String,
+	lat: String,
+	lng: String,
+	author: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	reviews: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'Review'
+		}
+	]
+});
+
+module.exports = mongoose.model('Post', PostSchema);
--- models/review.js
+++ models/review.js
@@ -1,5 +1,14 @@
-/*
-Review
-- body -array of objects
-- author -object id(ref User)
-*/
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+
+	body: String,
+    author: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	}
+});
+
+module.exports = mongoose.model('Review', ReviewSchema);
+
