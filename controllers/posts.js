0e0fb5ad73665b4f4aa69e102b5ca247c64b39fb
Benjo <beniamino.tartarini@gmail.com>
2019-12-19 01:19:55 +0900

    initialize img upload with multer

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,10 @@
 const Post = require("../models/post");
+const cloudinary = require("cloudinary");
+cloudinary.config({
+  cloud_name: "telriot",
+  api_key: "588434391324757",
+  api_secret: process.env.CLOUDINARY_SECRET
+});
 
 module.exports = {
   // Posts index
@@ -13,6 +19,14 @@ module.exports = {
 
   // Posts create
   async postCreate(req, res, next) {
+    req.body.post.images = [];
+    for (const file of req.files) {
+      let image = await cloudinary.v2.uploader.upload(file.path);
+      req.body.post.images.push({
+        url: image.secure_url,
+        public_id: image.public_id
+      });
+    }
     let post = await Post.create(req.body.post);
     res.redirect(`/posts/${post.id}`);
   },
@@ -31,13 +45,15 @@ module.exports = {
 
   // Posts update
   async postUpdate(req, res, next) {
-    let post = await Post.findByIdAndUpdate(req.params.id, req.body.post, {new: true});
+    let post = await Post.findByIdAndUpdate(req.params.id, req.body.post, {
+      new: true
+    });
     res.redirect(`/posts/${post.id}`);
   },
 
   //Posts delete
   async postDestroy(req, res, next) {
-      await Post.findByIdAndRemove(req.params.id)
-      res.redirect("/posts")
+    await Post.findByIdAndRemove(req.params.id);
+    res.redirect("/posts");
   }
 };
--- models/post.js
+++ models/post.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
-	title: String,
-	price: String,
-	description: String,
-	images: [ String ],
-	location: String,
-	lat: String,
-	lng: String,
-	author: {
-		type: Schema.Types.ObjectId,
-		ref: 'User'
-	},
-	reviews: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: 'Review'
-		}
-	]
+  title: String,
+  price: String,
+  description: String,
+  images: [{ url: String, public_id: String }],
+  location: String,
+  lat: String,
+  lng: String,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review"
+    }
+  ]
 });
 
-module.exports = mongoose.model('Post', PostSchema);
+module.exports = mongoose.model("Post", PostSchema);
